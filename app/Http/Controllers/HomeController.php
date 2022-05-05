49f40636ce7c6df110e4fcc25ed5c902ccbe0157
arfatS <arfatsarole@gmail.com>
2022-05-05 12:51:55 +0530

    home payment details

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,18 +36,41 @@ class HomeController extends Controller
         $total_customers = Customer::all()->count();
         $total_products = Product::all()->count();
 
+        $orders = Order::all();
+        $total_payments = $this->getTotalPayments($orders);
+
+        $orders_month = Order::whereMonth('created_at', date('m'))->get();
+        $total_payments_month = $this->getTotalPayments($orders_month);
+
+        $orders_year = Order::whereYear('created_at', date('Y'))->get();
+        $total_payments_year = $this->getTotalPayments($orders_year);
+
         return view('home', [
             'groups' => $groups,
             'total_orders' => $total_orders, 
             'total_orders_today' => $total_orders_today, 
             'total_customers' => $total_customers,
             'total_products' => $total_products,
-            'total_payments' => 0,
-            'total_payments_month' => 0,
-            'total_payments_year' => 0
+            'total_payments' => $total_payments,
+            'total_payments_month' => $total_payments_month,
+            'total_payments_year' => $total_payments_year
         ]);
     }
 
+    public function getTotalPayments($orders)
+    {
+        $total_payments = 0;
+        foreach ($orders as $order) {
+            $order_products = OrderProduct::where('order_id', $order->id)->get();
+            foreach ($order_products as $op) {
+                $pricing = Pricing::where('product_id', $op->product_id)->where('customer_id', $order->customer_id)->first();
+                if ($pricing) {
+                    $total_payments += ($pricing->price * $op->quantity);
+                }
+            }
+        }
+        return $total_payments;
+    }
 
     public function lang($locale){
 
